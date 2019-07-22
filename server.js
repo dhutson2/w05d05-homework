826076bae8842deadbd1d561a078eb72e405e29e
Derek Hutson <denverd1301@gmail.com>
2019-07-22 16:36:24 -0600

    Set up index landing page and index.ejs

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 
 require('./db/db')
+
+const rappersController = require('./models/rappers')
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use('/rappers', rappersController)
+
+app.get('/', (req,res) => {
+    res.render('index.ejs')
+})
 
 app.listen(3000, () => {
     console.log('listening on 3k')
